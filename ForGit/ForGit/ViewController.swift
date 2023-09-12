4b64dd7e490a443f33c51a19125ce1453b3371b2
arabirova <arabirova@inbox.ru>
2023-09-12 19:32:52 +0300

    initial commit

---
+++ ForGit/ForGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForGit
+//
+//  Created by Сашка on 12.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
